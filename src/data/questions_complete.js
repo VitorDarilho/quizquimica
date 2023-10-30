ddb4727325b75d21d5e67405085dea6f00ae0b9c
Vitor Darilho <vitorrodriguesdarilho@gmail.com>
2023-10-30 11:54:39 -0300

    main

---
+++ src/data/questions_complete.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    category: "Fenômenos de Transporte",
+    questions: [
+      {
+        question: "Um tanque rígido contém ar a 550kPa e 150ºC. Como resultado da transferência de calor para a vizinhança, a temperatura e a pressão caem 75ºC e 450kPa, respectivamente. Nessas condições, qual o trabalho de fronteira realizado durante esse processo?",
+        options: ["15kJ", "100kJ", "0", "-150kJ"],
+        answer: "0",
+      },
+      {
+        question: "A respeito das experiências de Reynolds, quanto aos comportamentos dos líquidos, assinalar a alternativa CORRETA:",
+        options: ["Renolds observou que, ao abrir gradualmente a torneira, pode-se observar a formação de um filamento colorido anfigúrico.", "No regime turbulento, a velocidade apresenta em qualquer instante um componente lamelar.", 
+        "O Melhor critério para se determinar o tipo de movimento em uma canalização consiste exclusivamente no valor da velocidade.", 
+        "Se o escoamento se verificar com R superior a 4.000, o movimento nas condições correntes, em tubos comerciais, sempre será turbulento."],
+        answer: "Se o escoamento se verificar com R superior a 4.000, o movimento nas condições correntes, em tubos comerciais, sempre será turbulento.",
+      },
+      {
+        question: "Uma tubulação industrial é feita de ferro e o engenheiro responsável deseja efetuar a proteção catódica nessa tubulação. Com base nessa informação, assinale a altenativa que corresponde a um metal que NÃO pode ser empregado como ânodo de sacrífício.",
+        options: ["Níquel", "Magnésio", "Alumínio", "Cromo"],
+        answer: "Níquel",
+      },
+      {
+        question: "Um reservatório de óleo, em forma de cilindro com diâmetro de 4 m e altura 8 m, está 2/3 cheio e precisa ser esvaziado. Para realizar o serviço, o gerente decidiu realizar a operação utilizando uma bomba com capacidade de 6 litros por segundo. Assinale a alternativa correta em relação ao tempo, em minuto, que a operação será executada.",
+        options: ["120", "140", "180", "200"],
+        answer: "120",
+      },
+      {
+        question: "A respeito de válvulas e acessórios de tubulações, indique a alternativa correta.",
+        options: ["A adição de válvulas nas tubulações não tem influência na perda de carga ao longo da linha.", "Válvulas de retenção são usadas para controlar níveis de vazão de fluidos", "Quando uma tubulação deve fazer uma curva, a melhor alternativa é dobrar o tubo, evitando-se o uso de acessórios e conexões.", "Enquanto a válvula esfera é uma válvula de bloqueio, a válvula globo permite um controle preciso da vazão de líquido em uma tubulação."],
+        answer: "Enquanto a válvula esfera é uma válvula de bloqueio, a válvula globo permite um controle preciso da vazão de líquido em uma tubulação.",
+      },
+    ],
+  },
+  {
+    category: "Química Analítica",
+    questions: [
+      {
+        question: "Sobre a técnica de espectrometria de massas (MS), assinale a alternativa correta.",
+        options: ["A espectrometria de massas (MS) consiste na ionização das moléculas de interesse e separação de íons pela diferença entre sua massa/carga, m/z.", 
+        "Pico Base(PB) é o sinal mais fraco do espectro e corresponde ao fragmento positivo menos estável.", "A espectrometria de massas envolve absorção e emissão de luz.", "Equivalentes de ligações duplas e anéis descrevem a medida do grau de insaturação de uma molécula orgânica dada por X - V/2 + Z/2+1, em que Y é o número de átomos de carbono, X é o número de átomos de hidrogênio ou de halogênio e Z é o número de átomos de nitrogênio e fósforo."],
+        answer: "A espectrometria de massas (MS) consiste na ionização das moléculas de interesse e separação de íons pela diferença entre sua massa/carga, m/z.",
+      },
+      {
+        question: "De acordo com Mendham (2008), os métodos da chamada “química por via úmida”, como a análise titrimétrica, ainda desempenham um importante papel na química analítica moderna. Sobre o uso das técnicas de titrimetria em laboratórios de controle de qualidade, assinale a opção correta.",
+        options: ["Em análise titrimétrica, a substância cuja concentração é conhecida é chamada de titulado e a substância que está sendo dosada é chamada de titulante.", "São exemplos de reações de oxidação-redução: dicromato de potássio, permanganato de potássio, sulfato de cério (IV) e carbonato de sódio.", 
+        "Solução padrão é uma solução cuja concentração não é conhecida com exatidão, e por isso precisa sofrer um processo de padronização química.", "Na determinação da dureza da água, utiliza-se a titulação potenciométrica por complexação com EDTA, sendo verificados dois pontos finais, um para o cálcio e outro para o magnésio."],
+        answer: "Na determinação da dureza da água, utiliza-se a titulação potenciométrica por complexação com EDTA, sendo verificados dois pontos finais, um para o cálcio e outro para o magnésio.",
+      },
+      {
+        question: "A técnica utilizada para a pesquisa dos antígenos da hepatite B, cujo princípio é a migração do HBsAg para o pólo positivo num campo eletroforético, ao mesmo tempo em que o anti-HBsAg migra para o pólo negativo. Uma linha de precipitação se formará no ponto em que o HBsAg e o anti-HBsAg se encontrarem em concentrações ótimas. Baseado nas provas sorológicas descritas porOliveira Lima (2001), como denomina-se a técnica acima descrita?",
+        options: ["Difusão em gel de agarose.", "Eletroforese em direções contrárias.", "Fixação do complemento.", " Radioimunoensaio"],
+        answer: "Eletroforese em direções contrárias.",
+      },
+    ],
+  },
+  
+];
+
+export default data;
